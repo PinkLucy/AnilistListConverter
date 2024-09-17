c5bf194d6462e12ce22ab4f1762abc3a871b6983
ElryMoe <gh@elry.moe>
2024-09-17 19:27:39 +0200

    Basic AniListNet Implementation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,36 +1,23 @@
 ﻿using System.Diagnostics;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using AniListNet;
+using AniListNet.Objects;
+using AniListNet.Parameters;
+using FuzzySharp;
+using Process = System.Diagnostics.Process;
 
 namespace AnilistListConverter;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-public partial class MainWindow : Window
+public partial class MainWindow
 {
-    private const string ClientId = "21239"; // Replace with your actual client ID
-    private const string AuthUrlTemplate = "https://anilist.co/api/v2/oauth/authorize?client_id={0}&response_type=token";
-
-    private State currentState = State.PreApi;
-
-    private enum State
-    {
-        PreApi,
-        ApiRequested,
-        ApiFailed,
-        ApiSuccess,
-        Working
-    }
-
+    private const string AuthUrl = "https://anilist.co/api/v2/oauth/authorize?client_id=21239&response_type=token";
+    
+    AniClient aniClient = new AniClient();
     
     public MainWindow()
     {
@@ -44,10 +31,9 @@ public partial class MainWindow : Window
 
     private void ApiButton_OnClick(object sender, RoutedEventArgs e)
     {
-        string authUrl = string.Format(AuthUrlTemplate, ClientId);
         Process.Start(new ProcessStartInfo
         {
-            FileName = authUrl,
+            FileName = AuthUrl,
             UseShellExecute = true
         });
 
@@ -55,43 +41,136 @@ public partial class MainWindow : Window
         ApiKey.IsEnabled = true;
         Confirm.Content = "Click to check Api Token";
         Confirm.IsEnabled = true;
-        currentState = State.ApiRequested;
+    }
+    
+    private void ToggleList_OnClick(object sender, RoutedEventArgs e)
+    {
+        bool toggle = ToggleList.IsChecked.GetValueOrDefault();
+        
+        if (toggle)
+        {
+            ToggleList.Content = "Move Anime to Manga";
+        }
+        else
+        {
+            ToggleList.Content = "Move Manga to Anime";
+        }
     }
 
-    private void ConfirmSettings_OnClick(object sender, RoutedEventArgs e)
+    private async void ConfirmSettings_OnClick(object sender, RoutedEventArgs e)
     {
-        Color color = (Color)ColorConverter.ConvertFromString("#8da888");
-        switch (currentState)
+        if (!ToggleList.IsEnabled)
         {
-            case State.ApiRequested:
-                Confirm.Width = 300;
-                Confirm.Background = new SolidColorBrush(color);
-                
-                
-                break;
-            case State.ApiFailed:
+            //Check the Api Key.
+            var result = await aniClient.TryAuthenticateAsync(ApiKey.Text);
+            if (!aniClient.IsAuthenticated)
+            {
                 Confirm.Width = 500;
                 Confirm.Content = "Token rejected, please try again with a new one.";
                 Confirm.Background = new SolidColorBrush(Colors.Coral);
-                break;
-            case State.ApiSuccess:
-                Confirm.Width = 300;
-                Confirm.Background = new SolidColorBrush(color);
-                break;
-            case State.Working:
-                Confirm.Width = 500;
-                Confirm.Background = new SolidColorBrush(Colors.Green);
-                break;
-            case State.PreApi:
-                Confirm.Width = 500;
-                Confirm.Content = "Something went horribly wrong, restart the program.";
-                Confirm.IsEnabled = false;
-                Confirm.Background = new SolidColorBrush(Colors.Red);
-                ApiButton.IsEnabled = false;
-                ApiKey.IsEnabled = false;
-                ToggleList.IsEnabled = false;
-                TogglePlannedAll.IsEnabled = false;
-                break;
+            }
+            else
+            {
+                ToggleList.IsEnabled = true;
+                Confirm.IsEnabled = true;
+                Confirm.Content = "Click to move Entries.";
+            }
+        }
+        else
+        {
+            //Move the Entries.
+            bool toggle = ToggleList.IsChecked.GetValueOrDefault();
+            MoveLists(toggle);
         }
     }
+    
+
+    private async void MoveLists(bool direction)
+    {
+        MediaType originalType;
+        MediaType newType;
+        
+        if (!direction)
+        {
+            originalType = MediaType.Manga;
+            newType = MediaType.Anime;
+        }
+        else
+        {
+            originalType = MediaType.Anime;
+            newType = MediaType.Manga;
+        }
+        
+        var user = await aniClient.GetAuthenticatedUserAsync();
+        
+        var mediaEntries = aniClient.GetUserEntriesAsync(user.Id).Result;
+
+        if (mediaEntries.Data.Length <= 0)
+        {
+            Confirm.Width = 500;
+            Confirm.Content = "Could not find User Lists.";
+            Confirm.Background = new SolidColorBrush(Colors.Coral);
+            return;
+        }
+        
+        Progress.Visibility= Visibility.Visible;
+
+        // ReSharper disable once PossibleLossOfFraction
+        Double progressPerEntry = 100 / mediaEntries.Data.Length ;
+        
+        Double currentProgress = 0;
+        
+
+        List<string?> notFound = new List<string?>();
+
+        foreach (var data in mediaEntries.Data)
+        {
+            currentProgress = currentProgress + progressPerEntry;
+            
+            Progress.Value = currentProgress;
+            
+            if (data.Media.Type != originalType)
+            {
+                continue;
+            }
+            
+            int newID = 0;
+            
+            var filter = new SearchMediaFilter
+            {
+                Query = data.Media.Title.NativeTitle,
+                Type = newType,
+                Sort = MediaSort.Popularity,
+                
+            };
+            var results = aniClient.SearchMediaAsync(filter, new AniPaginationOptions(1, 20)).Result;
+
+            if (results.TotalCount > 0)
+            {
+                foreach (var result in results.Data)
+                {
+                    int score = Fuzz.Ratio(result.Title.NativeTitle, data.Media.Title.NativeTitle);
+                    
+                    if (score >= 85)
+                    {
+                        newID = result.Id;
+                        break;
+                    }
+                }
+            }
+
+            var mutation = new MediaEntryMutation
+            {
+                // Set properties of the mutation object as needed
+                Status = MediaEntryStatus.Planning,
+                Progress = 0
+            };
+            
+            await aniClient.SaveMediaEntryAsync(newID,mutation);
+
+        }
+        
+        
+    }
+    
 }
